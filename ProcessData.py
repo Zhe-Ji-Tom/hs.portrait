98f01b3a90da95d64bbc4764a49bb451cb954995
Tom <404526222@qq.com>
2020-08-31 21:46:39 +0800

    split string

--- ProcessData.py
+++ ProcessData.py
@@ -4,6 +4,7 @@ import xlwt
 import xlutils
 from xlutils.copy import copy
 from WordCloud import generate_word_cloud
+import re
 
 generate_word_cloud("20200713163155845.xls", 20)
 #
@@ -39,5 +40,32 @@ generate_word_cloud("20200713163155845.xls", 20)
 # w_sheet.write(2, 17, 'test')
 # wb.save("20200713163155845.xls")
 
+# split string
+# split by '.'
+sentences = re.split(r'([0-9]?[0-9]\.)', cel_b2)
+model = 1
+# split by '、'
+if len(sentences) == 1:
+    sentences = re.split(r'([0-9]?[0-9]、)', cel_b2)
+    model = 2
+if len(sentences) == 1:
+    model = 0
 
+# remove meaningless sentences
+if model != 0:
+    for sen in sentences:
+        if sen == '':
+            sentences.remove(sen)
+    for sen in sentences:
+        if model == 1:
+            if "." in sen:
+                sentences.remove(sen)
+        else :
+            if "、" in sen:
+                sentences.remove(sen)
+print(sentences)
+
+for sen in sentences:
+    tags_pairs_TFIDF = jieba.analyse.extract_tags(sen, topK=5, withWeight=True, allowPOS=('v'))
+    print(tags_pairs_TFIDF)
 
