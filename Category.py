47afb38afb71153210fe266c0a2b523593967b4a
jizhe30321 <404526222@qq.com>
2020-10-09 14:20:53 +0800

    日报分析修改

--- Category.py
+++ Category.py
@@ -1,18 +1,17 @@
-import pymysql
 
 
-def get_tags_by_category(connect):
-    tags = {}
+def get_keywords_by_label(connect):
+    labels_keywords_map = {}
     cursor = connect.cursor()
     # 获取工作分类
-    cursor.execute("SELECT DISTINCT CATEGORY FROM CATEGORY_SET")
-    category_name = cursor.fetchall()
+    cursor.execute("SELECT ID FROM LABELS")
+    label_ids = cursor.fetchall()
 
-    for name in category_name:
-        cursor.execute("SELECT DISTINCT TAG FROM CATEGORY_SET WHERE CATEGORY = %s", name)
-        tag = cursor.fetchall()
-        tag_map = {name: tag}
-        tags.update(tag_map)
+    for label_id in label_ids:
+        cursor.execute("SELECT KEYWORD FROM KEYWORDS WHERE LABEL_ID = %s", label_id)
+        keywords = cursor.fetchall()
+        keyword_map = {label_id: keywords}
+        labels_keywords_map.update(keyword_map)
 
     connect.commit()
-    return tags
+    return labels_keywords_map
--- DailyReportProcess.py
+++ DailyReportProcess.py
@@ -1,6 +1,6 @@
 import pymysql
 import sys
-from Category import get_tags_by_category
+from Category import get_keywords_by_label
 
 # 数据源地址
 url = sys.argv[1]
@@ -20,22 +20,22 @@ def analyze():
         print("数据库连接失败！请检查数据库配置。")
         return
     # 获取工作分类及规则
-    categories = get_tags_by_category(connect)
+    keywords_map = get_keywords_by_label(connect)
     cursor = connect.cursor()
     cursor.execute("SELECT ID FROM USER")
     user_list = cursor.fetchall()
     for user in user_list:
         print("计算用户ID为{" + str(user[0]) + "}的词云")
         # 读取日报ID及内容
-        cursor.execute("SELECT ID, CONTENT FROM DAILY_REPORT WHERE USER_ID = %s ORDER BY TIME DESC LIMIT 1", user[0])
+        cursor.execute("SELECT ID, CONTENT FROM DAILY_REPORT WHERE USER_ID = %s ORDER BY REPORT_TIME DESC LIMIT 1", user[0])
         daily_report = cursor.fetchone()
-        for category in categories.keys():
-            tags = categories.get(category)
-            for tag in tags:
-                if tag in daily_report[1]:
+        for label in keywords_map.keys():
+            keywords = keywords_map.get(label)
+            for keyword in keywords:
+                if keyword in daily_report[1]:
                     # 记录日报工作类型
                     cursor.execute("INSERT INTO DAILY_REPORT_RESULT (DAILY_REPORT_ID, CATEGORY) VALUES (%d, %s)",
-                                   (daily_report[0], category))
+                                   (daily_report[0], label))
                     connect.commit()
                 break
 
